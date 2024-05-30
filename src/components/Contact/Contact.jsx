a4f4e63ff16b0201fce00d0afc8c269085bdac1c
blakh1 <148918293+blakh1@users.noreply.github.com>
2024-05-30 19:09:06 -0300

    contactus
    falta agregar una foto a la derecha

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -4,30 +4,28 @@ import "../../Styles/Contact.css";
 
 function Contact() {
   return (
-    <form
-      id="form"
-      className="text-center"
-      style={{ width: "100%", maxWidth: "300px" }}>
-      <h2>Contact us</h2>
+    <div className="contact">
+      <form id="form" className="text-center" style={{ width: "300px" }}>
+        <h2>Contact us</h2>
 
-      <MDBInput label="Name" v-model="name" wrapperClass="mb-4" />
+        <MDBInput label="Name" v-model="name" wrapperClass="mb-4" />
 
-      <MDBInput
-        type="email"
-        label="Email address"
-        v-model="email"
-        wrapperClass="mb-4"
-      />
+        <MDBInput
+          type="email"
+          label="Email address"
+          v-model="email"
+          wrapperClass="mb-4"
+        />
 
-      <MDBInput label="Subject" v-model="subject" wrapperClass="mb-4" />
+        <MDBInput label="Subject" v-model="subject" wrapperClass="mb-4" />
 
-      <MDBTextArea wrapperClass="mb-4" label="Message" />
+        <MDBTextArea wrapperClass="mb-4" label="Message" />
 
-  
-      <MDBBtn color="primary" block className="my-4">
-        Send
-      </MDBBtn>
-    </form>
+        <MDBBtn color="primary" block className="my-4">
+          Send
+        </MDBBtn>
+      </form>
+    </div>
   );
 }
 export default Contact;
