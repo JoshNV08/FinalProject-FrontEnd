30081e67aecca829669c7a3ab293fd262be7833d
JoshNV08 <yadidjoshua@gmail.com>
2024-05-31 19:06:52 -0300

    Update FilterList Responsive

--- src/components/Menu/FilterList.jsx
+++ src/components/Menu/FilterList.jsx
@@ -1,59 +1,42 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBurger } from '@fortawesome/free-solid-svg-icons';
-import { faBottleWater } from '@fortawesome/free-solid-svg-icons';
-import { faCookieBite } from '@fortawesome/free-solid-svg-icons';
-import { faIceCream } from '@fortawesome/free-solid-svg-icons';
-import { faMugHot } from '@fortawesome/free-solid-svg-icons';
+import { faBurger, faBottleWater, faCookieBite, faIceCream, faMugHot } from '@fortawesome/free-solid-svg-icons';
 
 function Filter() {
   return (
-      <div className="order  d-flex justify-content-center">
-      <div className="order rounded-1">
-        <ul className="d-flex justify-content-center gap-4 ">
-          <li className="">
+    <div className="order d-flex justify-content-center">
+      <div className="rounded-1">
+        <ul className="d-flex justify-content-center gap-3">
+          <li>
             <NavLink to="/menu/menu-burgers" activeClassName="active">
-    <div className="order  d-flex justify-content-center">
-      <div className="order rounded-1">
-        <ul className="d-flex justify-content-center gap-4 ">
-          <li className="">
-            <NavLink to="/menu/menu-burgers" activeClassName="active" >
-            <FontAwesomeIcon icon={faBurger} />
-              Burgers
+              <FontAwesomeIcon icon={faBurger} /> Burgers
             </NavLink>
           </li>
-          <li className="">
+          <li>
             <NavLink to="/menu/menu-snacks" activeClassName="active">
-            <FontAwesomeIcon icon={faCookieBite} />
-              Snacks
+              <FontAwesomeIcon icon={faCookieBite} /> Snacks
             </NavLink>
           </li>
-          <li className="">
+          <li>
             <NavLink to="/menu/menu-bebidas" activeClassName="active">
-
-            <FontAwesomeIcon icon={faBottleWater} />
-              Bebidas
+              <FontAwesomeIcon icon={faBottleWater} /> Bebidas
             </NavLink>
           </li>
-          <li className="">
+          <li>
             <NavLink to="/menu/menu-postres" activeClassName="active">
-            <FontAwesomeIcon icon={faIceCream} />
-
-              Postres
+              <FontAwesomeIcon icon={faIceCream} /> Postres
             </NavLink>
           </li>
-          <li className="">
+          <li>
             <NavLink to="/menu/menu-coffee" activeClassName="active">
-
-            <FontAwesomeIcon icon={faMugHot} />
-              Coffee
+              <FontAwesomeIcon icon={faMugHot} /> Coffee
             </NavLink>
           </li>
         </ul>
       </div>
     </div>
-  )
-  };
+  );
+}
 
 export default Filter;
