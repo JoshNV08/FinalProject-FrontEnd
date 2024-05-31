5c0db94fe593c207314cd245a8ed2a457714bfa5
JoshNV08 <yadidjoshua@gmail.com>
2024-05-31 18:45:21 -0300

    Update

--- src/components/Menu/FilterList.jsx
+++ src/components/Menu/FilterList.jsx
@@ -3,38 +3,37 @@ import { NavLink } from "react-router-dom";
 
 function Filter() {
   return (
-<div className="order  d-flex justify-content-center">
-    <div className="order rounded-1">
-    <ul className="d-flex justify-content-center gap-4 ">
-      <li className="">
-        <NavLink to="/menu/menu-burgers" activeClassName="active">
-  
-          Burgers
-        </NavLink>
-      </li>
-      <li className="">
-        <NavLink to="/menu/menu-snacks" activeClassName="active">
-          Snacks
-        </NavLink>
-      </li>
-      <li className="">
-        <NavLink to="/menu/menu-bebidas" activeClassName="active">
-          Bebidas
-        </NavLink>
-      </li>
-      <li className="">
-        <NavLink to="/menu/menu-postres" activeClassName="active">
-          Postres
-        </NavLink>
-      </li>
-      <li className="">
-        <NavLink to="/menu/menu-coffee" activeClassName="active">
-          Coffee
-        </NavLink>
-      </li>
-    </ul>
+      <div className="order  d-flex justify-content-center">
+      <div className="order rounded-1">
+        <ul className="d-flex justify-content-center gap-4 ">
+          <li className="">
+            <NavLink to="/menu/menu-burgers" activeClassName="active">
+              Burgers
+            </NavLink>
+          </li>
+          <li className="">
+            <NavLink to="/menu/menu-snacks" activeClassName="active">
+              Snacks
+            </NavLink>
+          </li>
+          <li className="">
+            <NavLink to="/menu/menu-bebidas" activeClassName="active">
+              Bebidas
+            </NavLink>
+          </li>
+          <li className="">
+            <NavLink to="/menu/menu-postres" activeClassName="active">
+              Postres
+            </NavLink>
+          </li>
+          <li className="">
+            <NavLink to="/menu/menu-coffee" activeClassName="active">
+              Coffee
+            </NavLink>
+          </li>
+        </ul>
+      </div>
     </div>
-    </div>  
   );
 }
 
