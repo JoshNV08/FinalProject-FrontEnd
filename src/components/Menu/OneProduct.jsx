c97eb83de417864b5923d1b9324e58e91b6bff5c
Joshua Nuñez <yadidjoshua@gmail.com>
2024-06-11 15:33:52 -0300

    Update Cart

--- src/components/Menu/OneProduct.jsx
+++ src/components/Menu/OneProduct.jsx
@@ -1,17 +1,26 @@
 import React, { useEffect, useState } from "react";
 import { Container, Row, Col, Form, Button } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
 import axios from "axios";
+import { useNavigate } from "react-router-dom"; // Importa useNavigate
 import "../../Styles/Buttons/ButtonsMenu.css";
+import { addToCart } from "../../features/Cart/cartSlice";
 
 function OneProduct({ id }) {
   const [product, setProduct] = useState(null);
+  const { items: products, status } = useSelector((state) => state.products);
+  const dispatch = useDispatch();
+  const navigate = useNavigate(); // Usa useNavigate
+
+  const handleAddToCart = (product) => {
+    dispatch(addToCart(product));
+    navigate("/cart"); // Usa navigate en lugar de history.push
+  };
 
   useEffect(() => {
     const fetchProduct = async () => {
       try {
-        const response = await axios.get(
-          `http://localhost:3000/products/${id}`
-        );
+        const response = await axios.get(`http://localhost:3000/products/${id}`);
         setProduct(response.data);
       } catch (error) {
         console.error("Error fetching the product:", error);
@@ -26,105 +35,107 @@ function OneProduct({ id }) {
   }
 
   function renderCustomizationOptions() {
-    return product.categoryId === 1 ? (
-      <>
-        <Form.Group className="mb-3">
-          <Form.Label>Quitar</Form.Label>
-          <Form.Select
-            name="removeOptions"
-            onChange={handleCustomizationChange}>
-            <option value="">Seleccione que desea sacar</option>
-            <option value="noLettuce">Sin Lechuga</option>
-            <option value="noTomato">Sin Tomate</option>
-            <option value="noMeat">Sin Carne</option>
-            <option value="noBread">Sin Pan</option>
-            <option value="noCondiments">Sin Condimentos</option>
-          </Form.Select>
-        </Form.Group>
-        <Form.Group className="mb-3">
-          <Form.Label>Extras</Form.Label>
-          <Form.Select name="addons" onChange={handleCustomizationChange}>
-            <option value="">Agrega un extra</option>
-            <option value="extraCheese">Extra Cheese</option>
-            <option value="bacon">Bacon</option>
-          </Form.Select>
-        </Form.Group>
-        <Form.Group className="mb-3">
-          <Form.Label>Indicación para su pedido</Form.Label>
-          <Form.Control
-            as="textarea"
-            name="specialInstructions"
-            placeholder="Te gustaría algo en especial?"
-            onChange={handleCustomizationChange}
-          />
-        </Form.Group>
-      </>
-    ) : product.categoryId === 3 ? (
-      <>
-        <Form.Group className="mb-3">
-          <Form.Label>Choose your Size</Form.Label>
-          <Form.Select name="size" onChange={handleCustomizationChange}>
-            <option value="zero">Zero</option>
-            <option value="original">Comun</option>
-            <option value="light">Light</option>
-          </Form.Select>
-        </Form.Group>
-        <Form.Group className="mb-3">
-          <Form.Label>Temperatura</Form.Label>
-          <Form.Select name="extras" onChange={handleCustomizationChange}>
-            <option value="natural">Natural</option>
-            <option value="cold">Fria</option>
-          </Form.Select>
-        </Form.Group>
-      </>
-    ) : product.categoryId === 2 ? (
-      <>
-        <Form.Group className="mb-3">
-          <Form.Label>Elija el tamaño</Form.Label>
-          <Form.Select name="size" onChange={handleCustomizationChange}>
-            <option value placeholder="">
-              Seleccione tamaño
-            </option>
-            <option value="zero">Mediano</option>
-            <option value="original">Grande</option>
-          </Form.Select>
-        </Form.Group>
-        <Form.Group className="mb-3">
-          <Form.Label>Extra</Form.Label>
-          <Form.Select name="extras" onChange={handleCustomizationChange}>
-            <option value placeholder="">
-              Seleccione extras
-            </option>
-            <option value="natural">Bacon</option>
-            <option value="cold">Cheddar</option>
-          </Form.Select>
-        </Form.Group>
-      </>
-    ) : product.categoryId === 5 ? (
-      <>
-        <Form.Group className="mb-3">
-          <Form.Label>Selecciona ingrediente</Form.Label>
-          <Form.Select name="size" onChange={handleCustomizationChange}>
-            <option value placeholder="">
-              Seleccione
-            </option>
-            <option value="zero">Azúcar</option>
-            <option value="original">Edulcorante</option>
-            <option value="light">Stevia</option>
-          </Form.Select>
-        </Form.Group>
-        <Form.Group className="mb-3">
-          <Form.Label>Tamaño</Form.Label>
-          <Form.Select name="extras" onChange={handleCustomizationChange}>
-            <option value placeholder="">
-              Seleccione tamaño
-            </option>
-            <option value="natural">Mediano</option>
-            <option value="cold">Grande</option>
-          </Form.Select>
-        </Form.Group>
-      </>
-    ) : null;
+    if (!product) return null;
+    switch (product.categoryId) {
+      case 1:
+        return (
+          <>
+            <Form.Group className="mb-3">
+              <Form.Label>Quitar</Form.Label>
+              <Form.Select name="removeOptions" onChange={handleCustomizationChange}>
+                <option value="">Seleccione que desea sacar</option>
+                <option value="noLettuce">Sin Lechuga</option>
+                <option value="noTomato">Sin Tomate</option>
+                <option value="noMeat">Sin Carne</option>
+                <option value="noBread">Sin Pan</option>
+                <option value="noCondiments">Sin Condimentos</option>
+              </Form.Select>
+            </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Label>Extras</Form.Label>
+              <Form.Select name="addons" onChange={handleCustomizationChange}>
+                <option value="">Agrega un extra</option>
+                <option value="extraCheese">Extra Cheese</option>
+                <option value="bacon">Bacon</option>
+              </Form.Select>
+            </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Label>Indicación para su pedido</Form.Label>
+              <Form.Control
+                as="textarea"
+                name="specialInstructions"
+                placeholder="Te gustaría algo en especial?"
+                onChange={handleCustomizationChange}
+              />
+            </Form.Group>
+          </>
+        );
+      case 3:
+        return (
+          <>
+            <Form.Group className="mb-3">
+              <Form.Label>Choose your Size</Form.Label>
+              <Form.Select name="size" onChange={handleCustomizationChange}>
+                <option value="zero">Zero</option>
+                <option value="original">Comun</option>
+                <option value="light">Light</option>
+              </Form.Select>
+            </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Label>Temperatura</Form.Label>
+              <Form.Select name="extras" onChange={handleCustomizationChange}>
+                <option value="natural">Natural</option>
+                <option value="cold">Fria</option>
+              </Form.Select>
+            </Form.Group>
+          </>
+        );
+      case 2:
+        return (
+          <>
+            <Form.Group className="mb-3">
+              <Form.Label>Elija el tamaño</Form.Label>
+              <Form.Select name="size" onChange={handleCustomizationChange}>
+                <option value="">Seleccione tamaño</option>
+                <option value="zero">Mediano</option>
+                <option value="original">Grande</option>
+              </Form.Select>
+            </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Label>Extra</Form.Label>
+              <Form.Select name="extras" onChange={handleCustomizationChange}>
+                <option value="">Seleccione extras</option>
+                <option value="natural">Bacon</option>
+                <option value="cold">Cheddar</option>
+              </Form.Select>
+            </Form.Group>
+          </>
+        );
+      case 5:
+        return (
+          <>
+            <Form.Group className="mb-3">
+              <Form.Label>Selecciona ingrediente</Form.Label>
+              <Form.Select name="size" onChange={handleCustomizationChange}>
+                <option value="">Seleccione</option>
+                <option value="zero">Azúcar</option>
+                <option value="original">Edulcorante</option>
+                <option value="light">Stevia</option>
+              </Form.Select>
+            </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Label>Tamaño</Form.Label>
+              <Form.Select name="extras" onChange={handleCustomizationChange}>
+                <option value="">Seleccione tamaño</option>
+                <option value="natural">Mediano</option>
+                <option value="cold">Grande</option>
+              </Form.Select>
+            </Form.Group>
+          </>
+        );
+      default:
+        return null;
+    }
   }
 
   return (
@@ -157,7 +168,7 @@ function OneProduct({ id }) {
             <Form className="p-3">
               <fieldset>
                 {renderCustomizationOptions()}
-                <Button className="btnMenu rounded-5 mt-3 w-100" href="/cart">
+                <Button className="btnMenu rounded-5 mt-3 w-100" onClick={() => handleAddToCart(product)}>
                   <span>Agregar al carrito</span>
                 </Button>
               </fieldset>
--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -1,4 +1,6 @@
-import Cart from "../components/Cart";
+
+import React from 'react';
+import Cart from '../components/Cart/Cart';
 
 function CartPage () {
     return (
