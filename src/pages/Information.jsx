3d8cd99085fef9091e5d6ee64caa58ee13558296
JoshNV08 <yadidjoshua@gmail.com>
2024-06-12 18:07:45 -0300

    Update Information.jsx

--- src/pages/Information.jsx
+++ src/pages/Information.jsx
@@ -1,7 +1,5 @@
-import { div } from "three/examples/jsm/nodes/Nodes.js";
 import Footer from "../components/Footer";
 import InfoExtend from "../components/Others/InfoExtend";
-import { Container, Row } from "react-bootstrap";
 
 function Information() {
   return (
