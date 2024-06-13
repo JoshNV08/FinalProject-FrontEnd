6f64f2c30bd18ab79d3d78d0e64eb640345ef1ca
Joshua Nuñez <yadidjoshua@gmail.com>
2024-06-13 15:47:22 -0300

    Update productsSlice.js

--- src/redux/productsSlice.js
+++ src/redux/productsSlice.js
@@ -3,38 +3,17 @@ import axios from "axios";
 
 const initialState = {
   items: [],
-  status: null,
 };
 
-export const productsFetch = createAsyncThunk(
-  "products/productsFetch",
-  async () => {
-    try {
-      const response = await axios.get("http://localhost:3000/products");
-      return response.data;
-    } catch (error) {
-      console.log(error);
-    }
-  }
-);
+export const productsFetch = createAsyncThunk("products/productsFetch", async () => {
+  const response = await axios.get("http://localhost:3000/products");
+  return response.data;
+});
 
 const productsSlice = createSlice({
   name: "products",
   initialState,
   reducers: {},
-  extraReducers: (builder) => {
-    builder
-      .addCase(productsFetch.pending, (state, action) => {
-        state.status = "pending";
-      })
-      .addCase(productsFetch.fulfilled, (state, action) => {
-        state.items = action.payload;
-        state.status = "success";
-      })
-      .addCase(productsFetch.rejected, (state, action) => {
-        state.status = "rejected";
-      });
-  },
 });
 
 export default productsSlice.reducer;
