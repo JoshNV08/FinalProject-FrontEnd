334f2565caa0f3756ba5098512a15cf89769a121
JoshNV08 <yadidjoshua@gmail.com>
2024-06-13 19:08:36 -0300

    Update productsSlice

--- src/redux/productsSlice.js
+++ src/redux/productsSlice.js
@@ -1,19 +1,19 @@
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-import axios from "axios";
+import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
   items: [],
 };
 
-export const productsFetch = createAsyncThunk("products/productsFetch", async () => {
-  const response = await axios.get("http://localhost:3000/products");
-  return response.data;
-});
-
 const productsSlice = createSlice({
   name: "products",
   initialState,
-  reducers: {},
+  reducers: {
+    setProducts: (state, action) => {
+      state.items = action.payload;
+    },
+  },
 });
+
+export const { setProducts } = productsSlice.actions;
 
 export default productsSlice.reducer;
